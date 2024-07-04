74266b7ab502f716f92e9a333bc756511b828baa
1f10230121 <s1f102301213@iniad.org>
2024-07-04 11:31:50 +0900

    flake8修正3

--- todo/tests.py
+++ todo/tests.py
@@ -52,7 +52,7 @@ class TodoViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data = {"title" : "Test Task", "due_at" : "2024-06-30 23:59:59"}
+        data = {"title": "Test Task", "due_at": "2024-06-30 23:59:59"}
         response = client.post("/", data)
 
         self.assertEqual(response.status_code, 200)
@@ -60,7 +60,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context["tasks"]), 1)
 
     def test_index_get_order_post(self):
-        task1 =  Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
         task2 = Task(title="task2", due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
